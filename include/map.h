9afedc5a4bdda89f79da846e7d1945d7877cb319
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2019-02-18 13:55:39 +0100

    [ADD] Adding new project strucutre

---
+++ include/map.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** map
+** File description:
+** map
+*/
+
+#ifndef MAP_H_
+#define MAP_H_
+
+typedef struct map {
+    char **map_player_one;
+    char **map_player_two;
+} map_t;
+
+#endif /* !MAP_H_ */
---
+++ src/map.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** map.c
+** File description:
+** Draw the map
+*/
+
+#include "my.h"
+#include "map.h"
+
+int print_map(void)
+{
+    char **map = NULL;
+
+    for (int i = 0; )
+}
