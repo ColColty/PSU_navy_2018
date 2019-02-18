69740b316cf7ba0275b82cdae41c138a0db06a8b
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2019-02-18 17:39:20 +0100

    [MODIFY] deploy.sh Trying to validate with a file instead of `echo "yes\n"`

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 echo ".travis.yml\n\
 deploy.sh\n\
-basic_tests.sh" >> .gitignore
+basic_tests.sh\n\
+validate.txt" >> .gitignore
 
-echo "yes\n" | git remote add blih git@git.epitech.eu:/paul.tardivel@epitech.eu/PSU_navy_2018
+git remote add blih git@git.epitech.eu:/paul.tardivel@epitech.eu/PSU_navy_2018 < validate.txt
 
-echo "yes\n" | git push blih master:master
+git push blih master:master < validate.txt
