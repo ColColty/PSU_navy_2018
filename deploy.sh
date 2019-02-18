39bb5bab17eb4f930bc5d95f899581c4daa3fc9c
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2019-02-18 17:35:16 +0100

    [MODIFY] deploy.sh Adding the entry to the yes for validate

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo ".travis.yml\n\
 deploy.sh\n\
 basic_tests.sh" >> .gitignore
 
-echo "yes" | git remote add blih git@git.epitech.eu:/paul.tardivel@epitech.eu/PSU_navy_2018
+echo "yes\n" | git remote add blih git@git.epitech.eu:/paul.tardivel@epitech.eu/PSU_navy_2018
 
-echo "yes" | git push blih master:master
+echo "yes\n" | git push blih master:master
