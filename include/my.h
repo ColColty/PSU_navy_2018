188a7fd078424677e01e9b89685411523f78a65c
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2019-02-20 00:48:50 +0100

    [MODIFY] *.h Resolving some merge errors

--- include/my.h
+++ include/my.h
@@ -28,7 +28,7 @@ typedef struct {
 void my_putchar(char c);
 int my_putstr(char const *str);
 int my_strlen(char const *str);
-int my_strcmp(char const *s1, char const *s2
+int my_strcmp(char const *s1, char const *s2);
 int my_show_word_array(char **tab);
 int my_put_nbr(int nb);
 
