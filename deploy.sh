03d2edee941087fe808fca85f59060691fa5b1da
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2019-02-19 14:09:04 +0100

    [MODIFY] deploy.sh Removing the file to validate, it doesn't work...

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 
 echo ".travis.yml\n\
 deploy.sh\n\
-basic_tests.sh\n\
-validate.txt" >> .gitignore
+basic_tests.sh" >> .gitignore
 
-git remote add blih git@git.epitech.eu:/paul.tardivel@epitech.eu/PSU_navy_2018 < validate.txt
+git remote add blih git@git.epitech.eu:/paul.tardivel@epitech.eu/PSU_navy_2018
 
-git push blih master:master < validate.txt
+git push blih master:master
