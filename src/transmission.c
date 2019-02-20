f2f4a387b6f04a2a307a720285ca2c881081eaab
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2019-02-20 11:52:52 +0100

    [MODIFY] transmission.c Sending and recieving binary codes

--- src/transmission.c
+++ src/transmission.c
@@ -6,27 +6,49 @@
 */
 
 #include "my.h"
+#include <stddef.h>
+#include <signal.h>
+#include <stdlib.h>
 
 void send_signal(connection_t *com, game_t *game)
 {
-    int binary = 0;
-
-    binary = my_send_nbr_base(game->user_input[0], "01", com);
+    my_send_nbr_base(game->user_input[0], "01", com);
     #ifdef TESTS
-        printf("$$ Signal for letter was send ! Binary: %d\n", binary);
+        printf("$$ Signal for letter was send !\n");
     #endif
-    binary = my_send_nbr_base(game->user_input[1], "01", com);
+    my_send_nbr_base(game->user_input[1], "01", com);
     #ifdef TESTS
-        printf("$$ Signal for number was sended ! Binary %d\n", binary);
+        printf("$$ Signal for number was sended !\n");
     #endif
 }
 
-static int signal_decoder(connection_t *com, game_t *game)
+void signal_decoder(int sig, siginfo_t *info, void *context)
 {
+    static char number[8];
+    static int i = 0;
+    int nb_binary = 0;
 
+    if (sig == 10)
+        number[i++] = '0';
+    else if (sig == 12)
+        number[i++] = '1';
+    if (i >= 7) {
+        number[i] = '\0';
+        my_revstr(number);
+        my_putstr(number);
+    }
 }
 
 void recieve_signal(connection_t *com, game_t *game)
 {
+    struct sigaction sa;
+    int sig[2] = {10, 12};
 
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = signal_decoder;
+    for (int i = 0; i < 8; i++) {
+        for (int k = 0; k < 2; k++)
+            sigaction(sig[k], &sa, NULL);
+        pause();
+    }
 }
