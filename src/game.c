43e26c4049536f8deb6727fbce09d993d1c90c19
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2019-02-20 09:21:51 +0100

    [MODIFY] game.c Lightweight the functions to do the connections

--- src/game.c
+++ src/game.c
@@ -12,31 +12,32 @@
 
 extern connection_t communication = {0, -1};
 
-static int connected(int signum)
+void get_attack_pid(int sig, siginfo_t *info, void *context)
 {
-    if (signum == 10)
+    communication.attack_pid = info->si_pid;
+    if (sig == 10)
         communication.connected = 1;
     return (0);
 }
 
-void get_attack_pid(int sig, siginfo_t *info, void *context)
+void connect_to_player(void)
 {
-    communication.attack_pid = info->si_pid;
-    connected(sig);
+    struct sigaction sa;
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = get_attack_pid;
+    sigaction(SIGUSR1, &sa, NULL);
+    while (!communication.connected);
 }
 
 void player1(char * const * argv, connection_t *com)
 {
-    struct sigaction sa;
 
     communication.pid = getpid();
     my_putstr("my_pid: ");
     my_put_nbr(communication.pid);
     my_putstr("\nWaiting for enemy connection...\n\n");
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = get_attack_pid;
-    sigaction(SIGUSR1, &sa, NULL);
-    while (!communication.connected);
+    connect_to_player();
     kill(communication.attack_pid, SIGUSR1);
     my_putstr("enemy connected\n");
     com = &communication;
@@ -44,19 +45,13 @@ void player1(char * const * argv, connection_t *com)
 
 void player2(char * const * argv, connection_t *com)
 {
-    int pid = atoi(argv[1]);
-    int mypid = getpid();
-    struct sigaction sa;
-
+    com->attack_pid = my_atoi(argv[1]);
+    com->pid = getpid();
     my_putstr("my_pid: ");
-    my_put_nbr(mypid);
+    my_put_nbr(com->pid);
     my_putchar('\n');
-    kill(pid, SIGUSR1);
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = get_attack_pid;
-    sigaction(SIGUSR1, &sa, NULL);
-    while (!communication.connected);
+    kill(com->attack_pid, SIGUSR1);
+    connect_to_player();
     my_putstr("successfully connected\n");
     com = &communication;
-    return (0);
 }
