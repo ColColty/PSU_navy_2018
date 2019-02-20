a167cf32f91bda59adb8191cd61c0df0c019bb0a
paul <paul.tardivel@epitech.eu>
2019-02-20 18:26:59 +0100

    [ADD] Two file for TWO players, more easy to use

---
+++ src/gestion_player_one.c
@@ -0,0 +1,77 @@
+/*
+** EPITECH PROJECT, 2019
+** gestion for the first player
+** File description:
+** player one gestion
+*/
+
+#include "include/my.h"
+#include "include/map.h"
+
+int change_line(char *position, int i)
+{
+    while (position[i] != '\n') {
+        if (position[i] == '\0')
+            return (i);
+            i++;
+    }
+    i += 1;
+    return (i);
+}
+
+char **add_position2(char **map, info_t *info)
+{
+    
+    int i = info->start_y - 1;
+    int k = info->start_x * 2;
+
+    while (k <= info->finish * 2) {
+        map[i][k] = info->nb_replace;
+        k += 2;
+    }
+    return (map);
+}
+
+char **add_position(char **map, info_t *info)
+{
+    int i = info->start_y - 1;
+    int k = info->start_x * 2;
+
+    while (i != info->finish) {
+        map[i][k] = info->nb_replace;
+        i++;
+    }
+    return (map);
+}
+
+char **get_position(char *position, info_t *info)
+{
+    int i = 0;
+    
+    while (position[i] != '\0') {
+        info->nb_replace = position[i];
+        i += 2;
+        if (position[i] == position[i + 3]) {
+            info->start_x = position[i] - 65;
+            info->start_y = position[i + 1] - 48;
+            info->finish = position[i + 4] - 48;
+            add_position(info->map, info);
+            i = change_line(position, i);
+        }
+        else { 
+            info->start_x = position[i] - 65;
+            info->start_y = position[i + 1] - 48;
+            info->finish = position[i + 3] - 65;
+            add_position2(info->map, info);
+            i = change_line(position, i);
+        }
+    }
+}
+
+int gestion_first_player(info_t *player_one, char *buffer)
+{
+    create_the_map(player_one);
+    get_position(buffer, player_one);
+    print_the_map(player_one->map, 1);
+    return (0);
+}
