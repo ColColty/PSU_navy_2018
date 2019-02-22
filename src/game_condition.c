f1a1d4ca3cfec931002b80bd7af82664939fac25
paul <paul.tardivel@epitech.eu>
2019-02-22 13:41:10 +0100

    [MODIF] Add function to find if the coor give touch a boat

--- src/game_condition.c
+++ src/game_condition.c
@@ -5,11 +5,26 @@
 ** WIning or losing condition
 */
 
+void touch_or_not(char *coor, info_t *player)
+{
+    for (int i = 0; player->map[i] != NULL; i ++) {
+        for (int k = 0; player->map[i][k] != '\0'; k++) {
+            if (player->map[coor[0]][coor[1] * 2] >= 1 && player->map[coor[0]][coor[1] * 2] <= 9) {
+                printf("TOUCH!");
+                return (0);
+            }
+        }
+    }
+    my_putstr("You don't touch");
+    return (-1)
+    
+}
+
 void game_condition(info_t *player)
 {
-    for (int i = 0; player.map[i] != NULL; i++) {
-        for (int k = 0; player.map[i][k] != '\0'; k++) {
-            if (player.map[i][k] >= 1 && player.map[i][k] <= 9)
+    for (int i = 0; player->map[i] != NULL; i++) {
+        for (int k = 0; player->map[i][k] != '\0'; k++) {
+            if (player->map[i][k] >= 1 && player->map[i][k] <= 9)
                 return (0);
         }
     }
