0b99eb31a8e3f45c5fe18ab3a18d2ccac676e808
paul <paul.tardivel@epitech.eu>
2019-02-26 18:22:51 +0100

    [MODIF] Add the proto of the main_error_function

--- include/map.h
+++ include/map.h
@@ -36,5 +36,6 @@ char *create_the_map(void);
 void print_hud(player_t *player);
 int gestion_first_player(info_t *player_defender, char *buffer);
 void create_map_defender(info_t *player);
+int main_error_gestion(char *pathfile);
 
 #endif
