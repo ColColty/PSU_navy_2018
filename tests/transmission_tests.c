e0449618bb4b7c7ada0b717224ef85545df50e73
tomas.forne-cappeau@epitech.eu <tomas.forne-cappeau@epitech.eu>
2019-03-03 09:52:32 +0100

    [MODIFY] transmission.c Adjusting times for the protocol Removing tests anti protocol

--- tests/transmission_tests.c
+++ tests/transmission_tests.c
@@ -31,44 +31,6 @@ Test(transmission_tests, transmit_letter_a)
     cr_assert_str_eq(ret, "01000001");
 }
 
-Test(transmission_tests, sending_a_signal_without_success)
-{
-    connection_t com = {0, 9970};
-    transmissions_t trans = {0, "01000001", "F1", "G3"};
-    int ret = 0;
-
-    ret = send_signal(&com, &trans);
-    cr_assert_eq(ret, -1);
-}
-
-Test(transmission_tests, sending_letter_and_number)
-{
-    connection_t com = {0, 9970};
-    transmissions_t trans = {0, "01000001", "F1", "G3"};
-    int ret = 0;
-    int pid = 0;
-    int subpid = 0;
-    struct sigaction sa = {0};;
-    int sigs[2] = {10, 12};
-
-    com.pid = getpid();
-    subpid = fork();
-    if (subpid == 0) {
-        sa.sa_flags = SA_SIGINFO;
-        sa.sa_sigaction = nothing_to_do;
-        for (int i = 0; i < 16; i++) {
-            for (int i = 0; i < 2; i++)
-                sigaction(sigs[i], &sa, NULL);
-            usleep(1000);
-            kill(com.pid, SIGCONT);
-        }
-    } else {
-        com.attack_pid = subpid;
-        ret = send_signal(&com, &trans);
-    }
-    cr_assert_eq(ret, 0);
-}
-
 Test(transmission_tests, connect_player1)
 {
     connection_t com = {0, 9970};
