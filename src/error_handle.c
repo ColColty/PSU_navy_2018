412bf42f2deca42c40a7e49b5b08b767308cfa4b
paul <paul.tardivel@epitech.eu>
2019-03-05 14:15:15 +0100

    [MODIF] Error handle

--- src/error_handle.c
+++ src/error_handle.c
@@ -7,66 +7,68 @@
 
 #include "map.h"
 #include "my.h"
+#include "error_handle.h"
 
-int switch_line(char *buffer, int i)
+int check_boat(char *buffer, int i, int choice, save_t *save_s)
 {
-    while(buffer[i] != '\n')
-        i++;
-    i += 1;
-    if (buffer[i + 1] == '\0')
-        return (i + 1);
-    else
-        return (i);
-}
+    int nbr = my_atoi_char(buffer[i + 6]) - my_atoi_char(buffer[i + 3]) + 1;
+    int letter = (buffer[i + 5] - 65) - (buffer[i + 2] - 65) + 1;
 
-int check_double_point(char *buffer)
+    if (choice == 1) {
+        if (nbr == save_s->nbr_save)
+            return (1);
+        else {
+            save_s->nbr_save = nbr;
+            return (0);
+        }
+    }
+    if (choice == 2) {
+        if (letter == save_s->letter_save)
+            return (1);
+        else {
+            save_s->letter_save = letter;
+            return (0);
+        }
+    }
+}   
+
+int check_size_boat(char *buffer, save_t *save_s)
 {
+    char save_letter;
     int i = 0;
 
-    while (buffer[i] != '\0') { 
-        if (buffer[i + 1] == ':' && buffer[i + 4] == ':')
-            i = switch_line(buffer, i);
+    save_s->nbr_save = 1;
+    save_s->letter_save = 1;
+    while (buffer[i] != '\0') {
+        if (buffer[i + 2] >= 65 && buffer[i + 2] <= 72
+        && my_atoi_char(buffer[i + 3]) >= 1 && my_atoi_char(buffer[i + 3]) <= 8) {
+            save_letter = buffer[i + 2];
+            if (buffer[i + 5] == save_letter && check_boat(buffer, i, 1, save_s) == 0)
+                i += 8;
+            else if (buffer[i + 5] != save_letter && check_boat(buffer, i, 2, save_s) == 0)
+                i += 8;
+            else 
+                return (1);
+        }
         else
-            return (-1);
+            return (1);
     }
     return (0);
 }
 
-int check_nbr(int save, int choice, char *buffer, int i)
+int verif_file(char *buffer)
 {
-    int temp = 0;
-
-    if (save >= 2 && save<= 5 && save != temp && choice == 0) {
-                temp = save;
-                i = switch_line(buffer, i);
-            }
-            else
-                return (1);
-    if (save + 1 >= 2 && save + 1 <= 5 && save + 1 != temp && choice == 1) {
-                temp = save;
-                i = switch_line(buffer, i);
-            }
-            else
-                return (1);
-    return (i);
-}
-
-int check_boat_size(char *buffer)
-{
+    int nbr = 2;
     int i = 0;
-    int save = 0;
 
     while (buffer[i] != '\0') {
-        if (buffer[i + 2] == buffer[i + 5]) {
-            save = (((buffer[i + 6] - 48) - (buffer[i + 3] - 48)) + 1);
-            my_put_nbr(save);
-            if (check_nbr(save, 0, buffer, i) == 1)
-                return (1);
+        if (my_atoi_char(buffer[i]) != nbr & buffer[i + 1] != ':' && buffer[i + 4] != ':') {
+            my_put_nbr(i);
+            return (1);
         }
         else {
-            save = ((buffer[i + 5] - buffer[i + 2]) + 1) - 65;
-            if (check_nbr(save, 1, buffer, i) == 1)
-                return (1);
+            i += 8;
+            nbr++;
         }
     }
     return (0);
@@ -91,6 +93,7 @@ int main_error_gestion(char *pathfile)
 {
     int fd = open(pathfile, O_RDONLY);
     char *buffer = NULL;
+    save_t save_s;
 
     if (fd == -1)
         return (1);
@@ -100,9 +103,9 @@ int main_error_gestion(char *pathfile)
     buffer[32] = '\0';
     if (check_content(buffer) != 32)
         return (1);
-    if (check_double_point(buffer) != 0)
+    if (verif_file(buffer) != 0)
         return (1);
-    if (check_boat_size(buffer) != 0)
+    if (check_size_boat(buffer, &save_s) != 0)
         return (1);
     free(buffer);
     return (0);
